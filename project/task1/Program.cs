eb42c4e5c68531c14e3c25f9aab92ac1b01c3da0
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-04-18 13:55:06 +0000

    wrote function prototype

--- project/task1/Program.cs
+++ project/task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] GetListOfTruncatedValues(string[] inboundList)
+{
+    string[] outboundList={};
+    int i = 0;
+    foreach(string element in inboundList)
+    {
+        if (element.Length > 3) continue;
+        outboundList[i] = element;
+        i++;
+    }
+    return outboundList;
+}
+
+string[] mylist = {"Hello", "2", "world", ":-)"};
+
+Console.WriteLine(GetListOfTruncatedValues(mylist));
+
