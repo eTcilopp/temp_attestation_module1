64fa259d6d887ad26843a892f2faaef5a4b38b70
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-04-19 00:46:14 +0000

    added limit as variable

--- project/task1/Program.cs
+++ project/task1/Program.cs
@@ -3,17 +3,17 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-List<string> GetListOfShortWords(List<string> inboundList)
+List<string> GetListOfShortWords(List<string> inboundList, int limit=3)
 {
     List<string> outboundList = new List<string>();
     foreach(string element in inboundList)
     {
-        if (element.Length <= 3) outboundList.Add(element);
+        if (element.Length <= limit) outboundList.Add(element);
     }
     return outboundList;
 }
 
-List<string> mylist = new List<string> {"Hello", "2", "world", ":-)"};
+List<string> mylist = new List<string> {"Hello", "2", "world", ":-)"}; // ["2", ":-)"]
 
 foreach(string word in GetListOfShortWords(mylist))
 {
